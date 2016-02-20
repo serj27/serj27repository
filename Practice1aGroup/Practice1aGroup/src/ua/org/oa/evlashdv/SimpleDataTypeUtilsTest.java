e38ca4153fa91b3b2c1c4c186848d4024b938317
dyachenko_s <dyachenko_s@37f2a86f-4dd6-e511-b04e-0030488c0138>
2016-02-20 10:06:13 +0000

    Share project "Practice1aGroup" into "http://oracle-academy.org.ua/svn/ATFEBXVIA"

---
+++ Practice1aGroup/Practice1aGroup/src/ua/org/oa/evlashdv/SimpleDataTypeUtilsTest.java
@@ -0,0 +1,17 @@
+package ua.org.oa.evlashdv;
+
+public class SimpleDataTypeUtilsTest {
+
+	public static void main (String[] args){
+		
+		System.out.println("~~~~Forth task test~~~~~");
+		SimpleDataTypeUtils.checkTriangleTask4 (2,3,4);
+		SimpleDataTypeUtils.displayTrueTask8(2, 2, 4);
+		//SimpleDataTypeUtils.calcMiddleTypeTask9(12, 8);
+		SimpleDataTypeUtils.sumIntegerResultTask5();
+		SimpleDataTypeUtils.sumNumbersTask6();
+		SimpleDataTypeUtils.sumPrimeNumbersTask7();
+		SimpleDataTypeUtils.calcMiddleTypeTask9(25, 6);
+		SimpleDataTypeUtils.creditBankTask10(3000, 0.2, 12);
+	}
+}
